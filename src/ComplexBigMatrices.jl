7dbd8770e0ea4b9efe9eea6b56c0577a4d6fab7f
Malay Das <malaydasat@gmail.com>
2021-09-22 05:52:24 -0700

    updated readme and removed eigen from the module

--- src/ComplexBigMatrices.jl
+++ src/ComplexBigMatrices.jl
@@ -23,21 +23,12 @@ end
 
 """
     log(a::Matrix{Complex{BigFloat}})
-Extends exp() to work with Matrix{Complex{BigFloat}}
+Extends log() to work with Matrix{Complex{BigFloat}}
 """
 function log(a::Matrix{Complex{BigFloat}})
     eig = eigen(a)
     eig.vectors*Diagonal(log.(eig.values))*eig.vectors^(-1)
 end
 
-
-"""
-    eigen(a::Matrix{Complex{BigFloat}})
-Extends exp() to work with Matrix{Complex{BigFloat}}
-
-function eigen(a::Matrix{Complex{BigFloat}})
-    eigen(a)
-end
-"""
 
 end
